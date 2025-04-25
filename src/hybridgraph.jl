68c82adf555a68edfc14a56ccd08751a21816c80
Simeon David Schaub <simeon@schaub.rocks>
2025-04-25 17:41:05 +0200

    add JET and AllocCheck tests
    also improve error handling to make JET happy

--- src/hybridgraph.jl
+++ src/hybridgraph.jl
@@ -13,21 +13,31 @@ Graphs.ne(g::HybridGraph) = g.ne
 Base.@propagate_inbounds Graphs.outneighbors(g::HybridGraph, i) = g.adj[i]
 Graphs.is_directed(::HybridGraph) = false
 
+@noinline function throw_add_edge_error(g::HybridGraph{N}, i, j) where {N}
+    throw(ArgumentError("Cannot add edge $i -> $j to graph $g. There are already $N edges for vertex $i"))
+end
+
 Base.@propagate_inbounds function Graphs.add_edge!(g::HybridGraph{N, W, T}, i::Integer, j::Integer, w::Real) where {N, W, T}
     (; adj, wts) = g
     adj′, wts′ = reinterpret(reshape, T, adj), reinterpret(reshape, W, wts)
 
     n = adj[i]
     k = findfirst(iszero, n)
+    k === nothing && throw_add_edge_error(g, i, j)
     adj′[k, i] = j
     wts′[k, i] = w
 
     n = adj[j]
     k = findfirst(iszero, n)
+    k === nothing && throw_add_edge_error(g, j, i)
     adj′[k, j] = i
     wts′[k, j] = w
 
     return HybridGraph{N, W, T}(adj, wts, g.ne + 2)
+end
+
+@noinline function throw_replace_error(g::HybridGraph, i, j)
+    throw(ArgumentError("Cannot replace edge $i -> $j in graph $g. The edge does not exist"))
 end
 
 Base.@propagate_inbounds function _replace!(g::HybridGraph{N, W, T}, i::Integer, j::Integer, j′::Integer, w′::Real) where {N, W, T}
@@ -36,6 +46,7 @@ Base.@propagate_inbounds function _replace!(g::HybridGraph{N, W, T}, i::Integer,
 
     n = adj[i]
     k = findfirst(==(j), n)
+    k === nothing && throw_replace_error(g, i, j)
     adj′[k, i] = j′
     wts′[k, i] = w′
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using RhombusTilings
-using Test
+using TestItemRunner
 
-@testset "RhombusTilings.jl" begin
-    # Write your tests here.
+@run_package_tests verbose = true
+
+@testitem "JET" begin
+    using JET
+
+    test_package("RhombusTilings")
+    test_call(shuffled_tiling, Tuple{NTuple{5, Int}, Int})
+end
+
+@testitem "AllocCheck" begin
+    using AllocCheck
+
+    @test isempty(check_allocs(RhombusTilings.shuffle!, Tuple{RhombusTiling{16}}))
 end
