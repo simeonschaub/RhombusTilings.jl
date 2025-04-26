732a9de3732584239f601f0d53993b6417ccbcb2
Simeon David Schaub <simeon@schaub.rocks>
2025-04-26 17:43:25 +0200

    add some more tests to improve coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,8 @@ using TestItemRunner
 
     t = shuffled_nflips(ntuple(_ -> 16, 5), 10^8)
     (; adj, vert) = t
+    @test nv(adj) == 2560
+    @test ne(adj) == 10080
     starting_points = findall(vertices(adj)) do i
         all(iszero, vert[i][2:end]) && 0x01 ∈ get_weight.(Ref(adj), i, neighbors(adj, i))
     end
@@ -28,6 +30,29 @@ using TestItemRunner
         end_pos = ntuple(j -> vert[i][j] + (j == last_step), 5)
         @test end_pos == ntuple(j -> j == 1 ? n - 1 : 16, 5)
     end
+
+    @test shuffled_tiling((5, 5, 5), 10^7) isa RhombusTiling
+end
+
+@testitem "HybridGraph" begin
+    using RhombusTilings: HybridGraph
+    using Graphs, SimpleWeightedGraphs
+
+    g = HybridGraph{2, Float64}(10)
+    @test nv(g) == 10
+    @test vertices(g) == 1:10
+    @test edgetype(g) == Int
+    @test ne(g) == 0
+    @test outneighbors(g, 1) == [0, 0]
+    @test !is_directed(g)
+
+    @test_throws ArgumentError rem_edge!(g, 1, 2)
+    g = add_edge!(g, 1, 2, 1.2)
+    g = add_edge!(g, 1, 3, 3.4)
+    @test_throws ArgumentError add_edge!(g, 1, 4, 5.6)
+    @test ne(g) == 4
+    @test outneighbors(g, 1) == [2, 3]
+    @test get_weight(g, 1, 2) == 1.2
 end
 
 @testitem "JET" begin
