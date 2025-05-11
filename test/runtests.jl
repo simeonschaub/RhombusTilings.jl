06d4e684ef00f049ef012420681ce9e23aedb08d
Simeon David Schaub <simeon@schaub.rocks>
2025-05-11 14:56:08 +0200

    increase coverage (#5)

--- test/runtests.jl
+++ test/runtests.jl
@@ -98,7 +98,7 @@ end
     @test length(p) == length(c)
 
     # TODO: add some better tests
-    @test plot(t) isa Makie.FigureAxisPlot
+    @test plot(t; swap_xy = true) isa Makie.FigureAxisPlot
     @test plot(sample_hahn_paths(2, 4, 2)) isa Makie.FigureAxisPlot
 end
 
