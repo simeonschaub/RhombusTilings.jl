64c4dfb93023df88b8c37f6049890b548942f96b
Simeon David Schaub <simeon@schaub.rocks>
2025-05-16 18:10:02 +0200

    wip `slice!` function

--- ext/MakieExtension.jl
+++ ext/MakieExtension.jl
@@ -13,7 +13,9 @@ function polys((; adj, vert)::RhombusTiling{N}, swap_xy = false) where {N}
     color = Int[]
     for (i, loc) in pairs(vert)
         origin = sum(loc .* basis)
-        i₁, i₂ = extrema(filter(!iszero, adj.wts[i]))
+        sides = filter(!iszero, adj.wts[i])
+        length(sides) < 2 && continue
+        i₁, i₂ = extrema(sides)
         a, b = basis[i₁], basis[i₂]
         push!(res, Polygon([origin, origin + a, origin + a + b, origin + b]))
 
