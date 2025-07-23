2f4f6d9dd2a9a8fee60e74782d2e6425c9539c31
Simeon David Schaub <simeon@schaub.rocks>
2025-07-23 11:01:47 +0200

    improve gpu code

--- gpu/gpu.jl
+++ gpu/gpu.jl
@@ -47,6 +47,19 @@ Base.@assume_effects :terminates_locally function binom(n::T, k::T) where {T <: 
     end
     return x
 end
+@generated function binom(n::T, k::T, ::Val{N}) where {T <: Integer, N}
+    table = SMatrix{N + 1, N + 1}(
+        Float32[binom(n, k) for k in 0:N, n in 0:N]
+    )
+    quote
+        if 0 ≤ n ≤ N && 0 ≤ k ≤ n
+            return $table[k + 1, n + 1]
+        else
+            return 0f0
+        end
+    end
+end
+
 @kernel function path_matrices!(
         A::AbstractArray{Float32, 4},
         @Const(src::AbstractVector{SVector{N, NTuple{2, I}}}),
@@ -56,12 +69,11 @@ end
     s, d = @inbounds src[j], dst[i]
     x_D, x_A = first.(s), first.(d)
     y_D, y_A = last.(s), last.(d)
-    inc = SizedVector{N}(I(0):I(N - 1))
     if all(x_D .≤ x_A) && all(y_D .≤ y_A)
         inc = SizedVector{N}(I(0):I(N - 1))
-        @inbounds @. A[:, :, i, j] = binom(x_A' - x_D + y_A' - y_D, x_A' - x_D + inc' - inc)
+        @inbounds @. A[:, :, i, j] = binom(x_A' - x_D + y_A' - y_D, x_A' - x_D + inc' - inc, Val(N))
     else
-        @inbounds @. A[:, :, i, j] .= 0
+        @inbounds A[:, :, i, j] .= 0f0
     end
 end
 
@@ -130,12 +142,13 @@ function compute_npaths(
     end
 
     dst, dst′, src, src′ = src, src′, dst, dst′
-    @allowscalar src[:, 1] .= Ref((I(0), I(0)))
+    src[:, 1] .= Ref((I(0), I(0)))
     path_matrices!(ROCBackend())(reshape(A, N, N, length(C), batch_size), src′, dst′; ndrange = (length(C), 1))
     batched_det!(det, view(A, :, :, 1:length(C)), info)
     det′ = view(det, 1:length(C))
     det′ .= log.(det′) .+ view(npaths, 1 .+ (1:length(C)))
-    @allowscalar npaths[1] = logsumexp(det′)
+    #@allowscalar npaths[1] = logsumexp(det′)
+    logsumexp2!(view(npaths, 1), det′, view(xmax_r, 1))
 
     return npaths
 end
