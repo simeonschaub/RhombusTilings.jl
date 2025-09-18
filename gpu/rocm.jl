af637fda23ed4eb84bfa68f64a09702a1f9f4693
Simeon David Schaub <simeon@schaub.rocks>
2025-09-18 11:45:25 +0200

    update GPU library versions

--- gpu/rocm.jl
+++ gpu/rocm.jl
@@ -23,21 +23,33 @@ DV = ROCMatrix{NTuple{2, I}}(
 )
 C = ROCVector(SVector{N}.(with_replacement_combinations(1:(2N + 1), N)))
 
-npaths = compute_npaths(DV, C)
+npaths = @time AMDGPU.@sync compute_npaths(DV, C)
 #path = sample_path(npaths, DV, C)
 #
 #src = ROCVector([SVector(ntuple(_ -> (I(0), I(0)), N))])
 #dst = reinterpret(reshape, SVector{N, NTuple{2, I}}, view(DV, :, 1)[reinterpret(reshape, Int, C)])
 #count_paths(src, dst)
 #
-c = let
-    src = reinterpret(reshape, SVector{N, NTuple{2, I}}, view(DV, :, 1)[reinterpret(reshape, Int, C)])[1:1000]
-    dst = reinterpret(reshape, SVector{N, NTuple{2, I}}, view(DV, :, 2)[reinterpret(reshape, Int, C)])
-    Slicing.count_paths(src, dst)
-end
 
-count(tuple.(dst, reshape(src, 1, :))) do (d, s)
-    x_D, x_A = first.(s), first.(d)
-    y_D, y_A = last.(s), last.(d)
-    all(x_D .≤ x_A) && all(y_D .≤ y_A)
-end / (length(dst) * length(src))
+DV7 = ROCMatrix{NTuple{2, I}}(
+    [
+        (0, 7)  (0, 7)  (0, 7)  (0, 7)  (0, 7)  (0, 7)  (0, 7)
+        (0, 6)  (0, 6)  (0, 6)  (0, 6)  (0, 6)  (0, 6)  (1, 7)
+        (0, 5)  (0, 5)  (0, 5)  (0, 5)  (1, 6)  (1, 6)  (2, 7)
+        (0, 4)  (0, 4)  (1, 5)  (1, 5)  (2, 6)  (2, 6)  (3, 7)
+        (0, 3)  (1, 4)  (2, 5)  (2, 5)  (2, 5)  (3, 6)  (4, 7)
+        (1, 3)  (1, 3)  (2, 4)  (3, 5)  (3, 5)  (3, 5)  (5, 7)
+        (1, 2)  (2, 3)  (3, 4)  (3, 4)  (3, 4)  (4, 5)  (5, 6)
+        (2, 2)  (3, 3)  (4, 4)  (4, 4)  (4, 4)  (4, 4)  (5, 5)
+        (3, 2)  (3, 2)  (4, 3)  (4, 3)  (5, 4)  (5, 4)  (6, 5)
+        (3, 1)  (3, 1)  (5, 3)  (5, 3)  (5, 3)  (6, 4)  (6, 4)
+        (4, 1)  (4, 1)  (6, 3)  (6, 3)  (6, 3)  (6, 3)  (6, 3)
+        (4, 0)  (4, 0)  (6, 2)  (6, 2)  (6, 2)  (6, 2)  (6, 2)
+        (5, 0)  (5, 0)  (6, 1)  (6, 1)  (6, 1)  (6, 1)  (6, 1)
+        (6, 0)  (6, 0)  (6, 0)  (6, 0)  (6, 0)  (7, 1)  (7, 1)
+        (7, 0)  (7, 0)  (7, 0)  (7, 0)  (7, 0)  (7, 0)  (7, 0)
+    ]
+)
+C7 = ROCVector(SVector{7}.(with_replacement_combinations(1:15, 7)))
+
+npaths7 = @time AMDGPU.@sync compute_npaths(DV7, C7, 10)
