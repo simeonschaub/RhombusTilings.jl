a49db6ffe6f35e76872fc2d07ecd4d9ffd3abd96
Documenter.jl <documenter@juliadocs.github.io>
2025-11-28 10:36:15 +0000

    build based on 27d1a40

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#RhombusTilings","page":"Home","title":"RhombusTilings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RhombusTilings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#RhombusTilings","page":"Home","title":"RhombusTilings","text":"Documentation for RhombusTilings.\n\n","category":"section"}]
 }
